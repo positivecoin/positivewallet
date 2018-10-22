e6ca55e071261c9dbb14c86dd020c538d9c868a3
positivecoin <44344371+positivecoin@users.noreply.github.com>
2018-10-22 20:59:04 +0200

    Update CryptoNoteWallet.cmake

--- CryptoNoteWallet.cmake
+++ CryptoNoteWallet.cmake
@@ -1,4 +1,4 @@
-
-set(CN_PROJECT_NAME "positive")
-set(CN_CURRENCY_DISPLAY_NAME "Positive")
-set(CN_CURRENCY_TICKER "Positive")
+set(CN_PROJECT_NAME "Positivecoin-GUI")
+set(CN_CURRENCY_DISPLAY_NAME "Positive-GUI")
+set(CN_CURRENCY_TICKER "PST")
+set(CN_VERSION 1.0.0)
